257c920613012f4341ae657ae5f4497e6b9e7257
moonstash <15162813+moonstash@users.noreply.github.com>
2020-12-01 14:07:42 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ class App extends React.Component {
       "secret"
     );
     const secretJsClient = new SecretJS.SigningCosmWasmClient(
-      "https://lcd.secretnodes.org",
+      "https://client.secretnodes.org/",
       myWalletAddress,
       (signBytes) => signingPen.sign(signBytes),
       tx_encryption_seed,
